f7b5010517004c7e360a1d1e9a3b844a7b9ceed2
JohnnyonFlame <johnnyonflame@hotmail.com>
2021-01-18 00:11:13 -0300

    Fix missing input on 210118 ArkOS update. Joystick gets detected as a gamepad, disabling the joystick path, so just force gamepad instead.

--- SP/assets/scripts/arkos/Return to Castle Wolfenstein.sh
+++ SP/assets/scripts/arkos/Return to Castle Wolfenstein.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+export SDL_GAMECONTROLLERCONFIG="03000000091200000031000011010000,OpenSimHardware OSH PB Controller,a:b0,b:b1,x:b2,y:b3,leftshoulder:b4,rightshoulder:b5,dpdown:h0.4,dpleft:h0.8,dpright:h0.2,dpup:h0.1,leftx:a0~,lefty:a1~,guide:b12,leftstick:b8,lefttrigger:b10,rightstick:b9,back:b7,start:b6,rightx:a2,righty:a3,righttrigger:b11,platform:Linux,"
 /roms/ports/iortcw/iowolfsp.aarch64
